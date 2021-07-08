623d0f3b072ef89a73eb92371ee10a181eaadfef
valdo <osvaelectro@gmail.com>
2021-07-08 15:06:08 -0500

    conectando nuestros microservicios con cliente eureka

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,11 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
