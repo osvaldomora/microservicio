fb5d9d9e6389813c6e782fddafbec4441a36e46d
valdo <osvaelectro@gmail.com>
2021-07-10 19:06:12 -0500

    librery commons was creatded and file Productos was deleted  from microservices

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,12 +2,14 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.formacionbdi.springboot.app.commons.models.entity"})//COMMENT TO TESTING
 public class SpringbootServicioProductosApplication {
 
 	public static void main(String[] args) {
