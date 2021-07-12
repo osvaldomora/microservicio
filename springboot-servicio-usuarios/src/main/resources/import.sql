e677299878ba8764e0fdfad659daaa32647701e3
valdo <osvaelectro@gmail.com>
2021-07-12 10:50:41 -0500

    obteniendo token jwt con postman se uso  app oauth del curso sin embargo se comenaron algunas clases y metodos, se llego hasta video 71, todo funcionando

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$bcnvZjWuMcFhJOjd8Qfq2eDxP1m1SaiIEh0aYYcX0VT6VXmPjX6xq',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
 INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
