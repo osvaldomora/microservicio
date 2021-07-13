6edc9b50af5e908867ad38f1910f1a4c761b4c99
valdo <osvaelectro@gmail.com>
2021-07-13 12:06:36 -0500

    dependency sleuth was added

---
+++ springboot-servicio-dependency/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioDependencyApplication.class, args);
+	}
+
+}
